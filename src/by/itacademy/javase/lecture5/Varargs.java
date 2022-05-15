d49474894f181efc35dabdd374bda36368cda3e9
Creseliana <valeria.sterzhanova@gmail.com>
2022-05-15 23:21:44 +0300

    lecture-5: varargs ambiguity example

--- src/by/itacademy/javase/lecture5/Varargs.java
+++ src/by/itacademy/javase/lecture5/Varargs.java
@@ -1,5 +1,7 @@
 package by.itacademy.javase.lecture5;
 
+import java.util.Arrays;
+
 /**
  * @author Valeria Sterzhanova
  */
@@ -8,6 +10,8 @@ public class Varargs {
     public static void main(String[] args) {
         printVarargs(1, 2, 11, 12, 22);
         printVarargs();
+//        varargsTypeAmbiguity(); // error
+//        varargsParamNumberAmbiguity(1, 2, 3, 4); // error
     }
 
     public static void printVarargs(int... numbers) {
@@ -20,5 +24,26 @@ public class Varargs {
             System.out.print(number + " ");
         }
         System.out.println();
+    }
+
+    public static void varargsTypeAmbiguity(int... numbers) {
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public static void varargsTypeAmbiguity(boolean... booleans) {
+        System.out.println(Arrays.toString(booleans));
+    }
+
+    public static void varargsTypeAmbiguity(char... chars) {
+        System.out.println(Arrays.toString(chars));
+    }
+
+    public static void varargsParamNumberAmbiguity(int... numbers) {
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public static void varargsParamNumberAmbiguity(int number, int... numbers) {
+        System.out.println(number);
+        System.out.println(Arrays.toString(numbers));
     }
 }
